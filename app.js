33fe197f9f8cc4b1fffa1c6b4c410a50f3b1be49
danhillida <dan.hill.ida@gmail.com>
2022-12-18 10:51:34 -0500

    Added Hamburger Menu

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 'use strict'
 
+/* ----- ELEMENTS ----- */
 const parallax = document.getElementById('parallax');
 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+/* ----- PARALLAX ----- */
 window.addEventListener('scroll', function() {
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
 })
+
+/* ----- HAMBURGER MENU ----- */
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}))
